76fb561ef33cb035640681f52933d4bd694b75f1
manuelcalerosolis <manuelcalerosolis@gmail.com>
2019-08-23 13:25:47 +0200

    purchase and sale prices

--- models/stock_move.py
+++ models/stock_move.py
@@ -23,7 +23,21 @@ class StockMove(models.Model):
         """
 
         for record in self:
-            for order_line in record.picking_id.sale_id.order_line:
+            for order_line in record.picking_id.purchase_id.order_line:
                 if order_line.product_id == record.product_id:
                     record.purchase_price_unit = order_line.price_total
 
+    sale_price_unit = fields.Float(
+        'Purchase price unit', compute='_compute_sale_price_unit',
+        digits=dp.get_precision('Product Price'),
+        readonly=True, help='Price that has already been assigned for this move')
+
+    @api.multi
+    def _compute_sale_price_unit(self):
+        """ Fill the `sale price` field on a stock move
+        """
+
+        for record in self:
+            for order_line in record.picking_id.sale_id.order_line:
+                if order_line.product_id == record.product_id:
+                    record.purchase_price_unit = order_line.price_total
