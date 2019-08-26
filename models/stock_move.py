df7ba97e0ccd6f55703909300877a6d5018d5c09
manuelcalerosolis <manuelcalerosolis@gmail.com>
2019-08-26 11:25:38 +0200

    compute move price

--- models/stock_move.py
+++ models/stock_move.py
@@ -12,32 +12,23 @@ class StockMove(models.Model):
     _inherit = "stock.move"
     _description = "Stock Move"
 
-    purchase_price_unit = fields.Float(
-        'Purchase price unit', compute='_compute_purchase_price_unit',
+    move_price_unit = fields.Float(
+        'Move price unit', compute='_compute_move_price_unit',
         digits=dp.get_precision('Product Price'),
         readonly=True, help='Price that has already been assigned for this move')
 
     @api.multi
-    def _compute_purchase_price_unit(self):
-        """ Fill the `purchase price` field on a stock move
+    def _compute_move_price_unit(self):
+        """ Fill the `move price` field on a stock move
         """
 
         for record in self:
-            for order_line in record.picking_id.purchase_id.order_line:
-                if order_line.product_id == record.product_id:
-                    record.purchase_price_unit = order_line.price_total
+            if len(record.picking_id.purchase_id) > 0:
+                for order_line in record.picking_id.purchase_id.order_line:
+                    if order_line.product_id == record.product_id:
+                        record.move_price_unit = order_line.price_total
 
-    sale_price_unit = fields.Float(
-        'Purchase price unit', compute='_compute_sale_price_unit',
-        digits=dp.get_precision('Product Price'),
-        readonly=True, help='Price that has already been assigned for this move')
-
-    @api.multi
-    def _compute_sale_price_unit(self):
-        """ Fill the `sale price` field on a stock move
-        """
-
-        for record in self:
-            for order_line in record.picking_id.sale_id.order_line:
-                if order_line.product_id == record.product_id:
-                    record.purchase_price_unit = order_line.price_total
+            if len(record.picking_id.sale_id) > 0:
+                for order_line in record.picking_id.sale_id.order_line:
+                    if order_line.product_id == record.product_id:
+                        record.move_price_unit = order_line.price_total
